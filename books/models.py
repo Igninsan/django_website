5f55ff7ad3809a847cda43ad1b1b9506c986ee02
Igninsan <zininp2007@gmail.com>
2025-02-08 16:28:39 +0600

    добавлены файлы

--- books/models.py
+++ books/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class BookModel(models.Model):
+    GENRE_CHOICES = (
+        ('FANTASY', 'FANTASY'),
+        ('MYSTERY', 'MYSTERY'),
+        ('ROMANCE', 'ROMANCE'),
+    )
+    image = models.ImageField(upload_to='films/', verbose_name='загрузите фото книги')
+    title = models.CharField(max_length=100, verbose_name='укажите название книги')
+    description = models.TextField(verbose_name='укажите описание книги', blank=True)
+    price = models.PositiveIntegerField(verbose_name='укажите цену книги', default=20)
+    created_at = models.DateField(auto_now_add=True)
+    genre = models.CharField(max_length=20, choices=GENRE_CHOICES, default='FANTASY',
+                             verbose_name='укажите жанр книги')
+    mail = models.CharField(max_length=100, verbose_name='укажите почту автора книги')
+    author = models.CharField(max_length=100, verbose_name='укажите автора книги', default='Чингиз Айтматов')
+    audio_book = models.URLField(verbose_name='укажите ссылку из YOUTUBE на аудиокнигу')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'книга'
+        verbose_name_plural = 'книги'
